0d4aa502f3fc6847fdd8a36a8a7d0ddfca1a55af
Viper2234 <97269693+Viper2234@users.noreply.github.com>
2025-04-24 23:12:09 -0500

    A card repository which adds the stat reset card.

---
+++ Cards/StatReset.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ModdingUtils.Extensions;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+
+namespace Stat_Reset.Cards
+{
+    class StatReset : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            UnityEngine.Debug.Log($"[{Stat_Reset.ModInitials}][Card] {GetTitle()} has been setup.");
+        }
+
+        public void ResetGunStats(Gun gun)
+        {
+            // Get the type of the Gun class
+            Type gunType = gun.GetType();
+
+            // Get the internal method ResetStats using reflection
+            MethodInfo resetStatsMethod = gunType.GetMethod("ResetStats", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (resetStatsMethod != null)
+            {
+                // Invoke the ResetStats method on the Gun object
+                resetStatsMethod.Invoke(gun, null);
+                Debug.Log("Gun stats have been reset.");
+            }
+            else
+            {
+                Debug.LogWarning("ResetStats method not found.");
+            }
+        }
+
+        public void ResetBlockStats(Block block)
+        {
+            // Get the type of the Gun class
+            Type blocktype = block.GetType();
+
+            // Get the internal method ResetStats using reflection
+            MethodInfo resetStatsMethod = blocktype.GetMethod("ResetStats", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (resetStatsMethod != null)
+            {
+                // Invoke the ResetStats method on the Gun object
+                resetStatsMethod.Invoke(block, null);
+                Debug.Log("Block stats have been reset.");
+            }
+            else
+            {
+                Debug.LogWarning("ResetStats method not found.");
+            }
+        }
+
+        public void ResetStatModifiyingStats(CharacterStatModifiers statModifiers)
+        {
+            // Get the type of the Gun class
+            Type statModifierstype = statModifiers.GetType();
+
+            // Get the internal method ResetStats using reflection
+            MethodInfo resetStatsMethod = statModifierstype.GetMethod("ResetStats", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (resetStatsMethod != null)
+            {
+                // Invoke the ResetStats method on the Gun object
+                resetStatsMethod.Invoke(statModifiers, null);
+                Debug.Log("Block stats have been reset.");
+            }
+            else
+            {
+                Debug.LogWarning("ResetStats method not found.");
+            }
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+
+            // Reset stats for the gun using reflection
+            if (gun != null)
+            {
+                ResetGunStats(gun);
+            }
+
+            if (block != null)
+            {
+                ResetBlockStats(block);
+            }
+
+            if (characterStats != null)
+            {
+                ResetStatModifiyingStats(characterStats);
+            }
+        }
+
+        protected override string GetTitle()
+        {
+            return "Stat Reset";
+        }
+        protected override string GetDescription()
+        {
+            return "Set all stats to their default values";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "All Stats",
+                    amount = "Default",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return Stat_Reset.ModInitials;
+        }
+    }
+}
---
+++ StatReset.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using Stat_Reset.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace Stat_Reset
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class Stat_Reset : BaseUnityPlugin
+    {
+        private const string ModId = "com.viper2234.rounds.statreset";
+        private const string ModName = "Stat Reset Card";
+        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SR";
+
+        public static Stat_Reset instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<StatReset>(); // Repeat for each card to be built
+        }
+    }
+}
